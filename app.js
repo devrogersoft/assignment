f8805c3d53e904991fdb364f26c47e75365ece8a
devrogersoft <devrogersoft@gmail.com>
2024-08-28 20:56:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const { getDb } = require('./util/mongoconnect'); 
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(async (req, res, next) => {
+    try {
+      req.db = await getDb();
+      next();
+    } catch (err) {
+      res.status(500).send('Database connection error');
+    }
+  });
+  
+
+app.use('/students', require('./routes/students'));
+
+app.use(async (req, res, next) => {
+  try {
+    req.db = await getDb();
+    next();
+  } catch (err) {
+    res.status(500).send('Database connection error');
+  }
+});
+
+// Homepage
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Student Management' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
