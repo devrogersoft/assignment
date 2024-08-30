38c0e978b3e4df497ba08e88ddfef2fd3ff05cea
Jibin T J <jibintj6351@gmail.com>
2024-08-30 20:16:59 +0530

    error fix

--- routes/students.js
+++ routes/students.js
@@ -1,40 +1,59 @@
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
+const { ObjectId } = require('mongodb');
 
 
-router.get('/', (req, res) => {
-  req.db.collection('students').find().toArray((err, result) => {
-    if (err) return console.error(err);
-    res.render('students', { title: 'Students List', students: result });
-  });
+router.get('/', async(req, res) => {
+
+  try {
+    const data=await req.db.collection('students').find().toArray()
+       res.render('students', { title: 'Students List', students: data });
+   
+  } catch (error) {
+    console.log(error)
+  }
+
+
+
 });
 
 router.get('/add', (req, res) => {
   res.render('add_student', { title: 'Add Student' });
 });
 
-router.post('/add', (req, res) => {
-  const student = {
-    name: req.body.name,
-    age: req.body.age,
-    class: req.body.class,
-  };
+router.post('/add', async(req, res) => {
 
-  req.db.collection('students').insertOne(student, (err, result) => {
-    if (err) return console.error(err);
-    console.log('Student Added');
-    res.redirect('/students');
-  });
-});
+  try {
+    const student = {
+      name: req.body.name,
+      age: req.body.age,
+      class: req.body.class,
+    };
+  
+    await req.db.collection('students').insertOne(student)
+      console.log('Student Added');
+      res.redirect('/students');
+  
 
-router.post('/delete/:id', (req, res) => {
-  const id = new require('mongodb').ObjectID(req.params.id);
-  req.db.collection('students').deleteOne({ _id: id }, (err, result) => {
-    if (err) return console.error(err);
-    console.log('Student Deleted');
-    res.redirect('/students');
+  } catch (error) {
+    
+  }
+})
+
+
+router.post('/delete/:id', async(req, res) => {
+  try {
+    const id = new ObjectId(req.params.id);
+    await req.db.collection('students').deleteOne({ _id: id })
+    
+      console.log('Student Deleted');
+      res.redirect('/students');
+  } catch (error) {
+    console.log(error)
+  }
+
   });
-});
+
 
 module.exports = router;
